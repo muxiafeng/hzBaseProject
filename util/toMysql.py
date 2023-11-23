316e9284cdbcb39f6917a8308d0b6bb79caef9bb
muxiafeng <862365198@qq.com>
2023-11-23 18:20:24 +0800

    update tomysql

--- util/toMysql.py
+++ util/toMysql.py
@@ -1,21 +1,36 @@
 import pymysql
 
 
-def tomysql():
-    conn = pymysql.connect(
-        host='117.50.187.15',
-        port=3306,
-        user='root',
-        password='mysql123?',
-        database='testPrepare',
-        charset='utf8'
-    )
-    print(conn)
-    cursor = conn.cursor()
-    result = cursor.execute('select count(1) from testPrepare.personinfo limit 10')
-    print(result)
-    conn.close()
+def linkMysql(excuteSql):
+    # 创建连接
+    conn = pymysql.connect(host='117.50.187.15',  # 你的数据库地址
+                           port=3306,
+                           user='root',  # 你的数据库用户名
+                           password='mysql123?',  # 你的数据库密码
+                           db='testPrepare',  # 你的数据库名称
+                           charset='utf8mb4',  # (可选) 数据库字符集,默认是utf8mb4
+                           cursorclass=pymysql.cursors.DictCursor)  # 使用字典形式返回查询结果
+
+    try:
+        with conn.cursor() as cursor:
+            # 执行SQL语句
+            sql = excuteSql  # 替换为你的查询语句
+            cursor.execute(sql)
+            result = cursor.fetchall()  # 获取查询结果
+            return result
+            # for row in result:  # 遍历查询结果
+            #     print(row)  # 打印每一行结果
+    finally:
+        conn.close()  # 关闭连接
 
 
 if __name__ == '__main__':
-    tomysql()
+    result = linkMysql('select count(1) from personinfo')
+    print(result)
+    for row in result:  # 遍历查询结果
+        print(row)
+
+    result1 = linkMysql('select * from personinfo limit 0,5')
+    print(result1)
+    for row1 in result1:  # 遍历查询结果
+        print(row1)
