84b6839a9528bec7684d0a214c0a0b47615c7696
muxiafeng <862365198@qq.com>
2023-12-03 16:44:04 +0800

    update order

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-nohup python3 app.py &
-
+nohup python3 /pythonProject/hzBaseProject/app.py > /pythonProject/hzBaseProject/baseProject.log 2>&1 &
--- stop.sh
+++ stop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -ef | grep app.py | awk '{print $2}' | xargs kill -9
+ps -ef | grep app.py | grep -v grep | awk '{print $2}' | xargs kill -9
