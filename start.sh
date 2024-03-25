402bf0d5f1d2224e978ef6d5370059a220f40b02
muxiafeng <862365198@qq.com>
2024-03-25 10:03:59 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+pip_list="$(pip list)"
+# shellcheck disable=SC1089
+if! echo "$pip_list" | grep -q "request"; then
+    pip install -r requirement.txt
+fi
+
 nohup python3 /pythonProject/hzBaseProject/app.py > /pythonProject/hzBaseProject/baseProject.log 2>&1 &
